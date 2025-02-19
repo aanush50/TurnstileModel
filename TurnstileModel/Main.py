8f1945722f6a3334fee18569115afde9d731fd60
aanush50 <ako.anush@gmail.com>
2025-02-19 21:33:22 +0300

    Add files via upload

---
+++ TurnstileModel/Main.py
@@ -0,0 +1,217 @@
+﻿from inspect import isclass
+import random
+from time import sleep
+import pygame
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
+
+from TurnstileModel import TurnstileModel
+
+from TurnstileModel import PedestrianAgent
+from TurnstileModel import TurnstileAgentIn
+from TurnstileModel import TurnstileAgentOut
+
+from matplotlib.figure import Figure
+from mesa.experimental import JupyterViz
+pygame.init()
+
+width = 1000
+height = 850 
+
+#Запуск имитационной модели MESA
+
+
+# Set up the drawing window
+screen = pygame.display.set_mode([width, height])
+
+
+slider1 = Slider(screen, 50, 25, 800, 10, min=0, max=300, step=1)
+output1 = TextBox(screen, 875, 15, 50, 25, fontSize=20)
+output1.disable() 
+
+slider2 = Slider(screen, 50, 50, 800, 10, min=1, max=10, step=1)
+output2 = TextBox(screen, 875, 40, 50, 25, fontSize=20)
+output2.disable() 
+
+slider3 = Slider(screen, 50, 75, 800, 10, min=1, max=10, step=1)
+output3 = TextBox(screen, 875, 60, 50, 25, fontSize=20)
+output3.disable() 
+
+slider4 = Slider(screen, 50, 100, 800, 10, min=30, max=54, step=1)
+output4 = TextBox(screen, 875, 80, 50, 25, fontSize=20)
+output4.disable() 
+
+slider1.setValue(300); # Количество агентов-пешеходов (от 100 до 300)
+slider2.setValue(4); # Количество турникетов на вход (от 1 до 10)
+slider3.setValue(4); # Количество турникетов на выход (от 1 до 10)
+slider4.setValue(50); # Расстояние между турникетами в px (от 30 до 50)
+
+starter_model = TurnstileModel(slider1.value, slider2.value, slider3.value, slider4.value, width, height)
+
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+text = font.render('Total pedestrian outflow', True, green, blue)
+text2 = font.render('Time', True, green, blue)
+text3 = font.render('Enter', True, green, blue)
+text4 = font.render('Exit', True, green, blue)
+
+textRect = text.get_rect()
+textRect.center = (260, 170)
+
+textRect2 = text2.get_rect()
+textRect2.center = (900, 170)
+
+textRect3 = text3.get_rect()
+textRect3.center = (320, 450)
+
+textRect4 = text4.get_rect()
+textRect4.center = (500, 450)
+
+output = TextBox(screen, 475, 145, 50, 50, fontSize=30)
+output.disable() 
+
+output_t = TextBox(screen, 800, 145, 50, 50, fontSize=30)
+output_t.disable() 
+
+# Run until the user asks to quit
+running = True
+
+# Количество пешеходов которые прошли через турникеты 
+ped_out_flow = 0
+
+t = 1
+Total_time = 100
+
+while t <= Total_time:
+
+    pygame.time.delay(100) # Время задержки кадра
+    
+    if pygame.mouse.get_pressed()[0] != 0:
+        # collision detection also needed here
+        a = pygame.mouse.get_pos()[0] - 5
+        if a < 0:
+            a = 0
+    
+   
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    #pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Управление количеством агентов-пешеходов
+    delta_agents1 = slider1.getValue() - len(starter_model.get_agents_of_type(PedestrianAgent))
+    
+    if delta_agents1 > 0:
+        for i in range(0, delta_agents1):
+            starter_model.add_agent1()
+            
+    
+    if delta_agents1 < 0:
+        while slider1.getValue() -  len(starter_model.get_agents_of_type(PedestrianAgent)) < 0:
+            i_rez = int(random.random()* len(starter_model.get_agents_of_type(PedestrianAgent)))
+            agent = starter_model.get_agents_of_type(PedestrianAgent)[i_rez]
+
+            if isinstance(agent, PedestrianAgent):
+                starter_model.schedule.remove(agent)
+                agent.remove()
+                
+    distance = slider4.value # Расстояние между турникетами 
+    # Управление количеством агентов-турникетов на вход
+    delta_agents2 = slider2.getValue() - len(starter_model.get_agents_of_type(TurnstileAgentIn))
+    
+    if delta_agents2 > 0:
+        for i in range(0, delta_agents2):
+            turn_count = len(starter_model.get_agents_of_type(TurnstileAgentIn))
+            x_last = starter_model.get_agents_of_type(TurnstileAgentIn)[turn_count-1].x
+            y_last = starter_model.get_agents_of_type(TurnstileAgentIn)[turn_count-1].y
+            starter_model.add_agent2(y_last, distance)
+            
+    
+    if delta_agents2 < 0:
+        while slider2.getValue() -  len(starter_model.get_agents_of_type(TurnstileAgentIn)) < 0:
+            i_rez = len(starter_model.get_agents_of_type(TurnstileAgentIn)) - 1
+            agent = starter_model.get_agents_of_type(TurnstileAgentIn)[i_rez]
+
+            if isinstance(agent, TurnstileAgentIn):
+                starter_model.schedule.remove(agent)
+                agent.remove()
+                starter_model.arr_list1.pop(i_rez-1)
+                
+ # Управление количеством агентов-турникетов на выход
+    delta_agents3 = slider3.getValue() - len(starter_model.get_agents_of_type(TurnstileAgentOut))
+    
+    if delta_agents3 > 0:
+        for i in range(0, delta_agents3):
+            turn_count = len(starter_model.get_agents_of_type(TurnstileAgentOut))
+            x_last = starter_model.get_agents_of_type(TurnstileAgentOut)[turn_count-1].x
+            y_last = starter_model.get_agents_of_type(TurnstileAgentOut)[turn_count-1].y
+            starter_model.add_agent3(y_last, distance)
+            
+    
+    if delta_agents3 < 0:
+        while slider3.getValue() -  len(starter_model.get_agents_of_type(TurnstileAgentOut)) < 0:
+            i_rez = len(starter_model.get_agents_of_type(TurnstileAgentOut)) - 1
+            agent = starter_model.get_agents_of_type(TurnstileAgentOut)[i_rez]
+
+            if isinstance(agent, TurnstileAgentOut):
+                starter_model.schedule.remove(agent)
+                agent.remove()
+                starter_model.arr_list2.pop(i_rez-1)
+
+    pedestrian_agents = starter_model.get_agents_of_type(PedestrianAgent)
+    turnistile_agents_in = starter_model.get_agents_of_type(TurnstileAgentIn)
+    turnistile_agents_out = starter_model.get_agents_of_type(TurnstileAgentOut)
+
+    for i in range(0, len(pedestrian_agents)):
+        screen.blit(pedestrian_agents[i].human, (pedestrian_agents[i].x, pedestrian_agents[i].y))
+        if ((pedestrian_agents[i].x > 450 and pedestrian_agents[i].type == 1) or (pedestrian_agents[i].x < 450 and pedestrian_agents[i].type == 2)) and pedestrian_agents[i].state == 0:
+            pedestrian_agents[i].state = 1
+            ped_out_flow += 1 
+            
+        
+    for i in range(0, len(turnistile_agents_in)):
+        screen.blit(turnistile_agents_in[i].turn, (turnistile_agents_in[i].x, turnistile_agents_in[i].y))
+    
+    for i in range(0, len(turnistile_agents_out)):
+        screen.blit(turnistile_agents_out[i].turn, (turnistile_agents_out[i].x, turnistile_agents_out[i].y))
+       
+    # Шаг модели
+    starter_model.step()
+    
+    screen.blit(text, textRect)
+    screen.blit(text2, textRect2)
+    screen.blit(text3, textRect3)
+    screen.blit(text4, textRect4)
+    output.setText(ped_out_flow)
+    
+    output1.setText(slider1.getValue())
+    output2.setText(slider2.getValue())
+    output3.setText(slider3.getValue())
+    output4.setText(slider4.getValue())
+    
+    pygame_widgets.update(event)
+
+    # Отрисовка помещения
+    pygame.draw.line(screen, (0, 0, 0), (420, 200), (420, 800))
+    pygame.draw.line(screen, (0, 0, 0), (850, 200), (850, 800))
+    pygame.draw.line(screen, (0, 0, 0), (420, 200), (850, 200))
+    pygame.draw.line(screen, (0, 0, 0), (420, 800), (850, 800))
+           
+    t = t + 1
+    output_t.setText(t)
+    # Flip the display
+    if t>=Total_time:
+       pygame.time.wait(10000)
+    pygame.display.flip()
+# Done! Time to quit.
+pygame.quit()
---
+++ TurnstileModel/TurnstileModel.py
@@ -0,0 +1,291 @@
+﻿from math import atan2, cos, sin
+from re import A
+from turtle import distance
+import mesa
+from mesa.space import F, ContinuousSpace
+import random
+import math
+
+# Data visualization tools.
+# import seaborn as sns
+
+# Has multi-dimensional arrays and matrices. Has a large collection of
+# mathematical functions to operate on these arrays.
+import numpy as np
+import pygame
+
+# Data manipulation and analysis.
+# import pandas as pd
+
+class PedestrianAgent(mesa.Agent): #Класс агента
+    """An agent with fixed initial wealth."""
+  
+    def __init__(self, unique_id, type, model):
+        # Pass the parameters to the parent class.
+        super().__init__(unique_id, model)
+
+        # Create the agent's attribute and set the initial values.
+        self.human = pygame.image.load('human.png')
+        self.type = type # Направление движение агента-пешехода: 1-на вход, 2 - на выход
+        self.state = 0 # Состояние агента-пешехода: 0-еще не прошел турникет, 1 - прошел турникет
+        self.x = 0
+        self.y = 0
+
+    def step(self):
+        
+        #Оцениваем количество агентов расположенных в области с фиксированным радиусом R независимо от направления их движения
+        R = 100
+        po = 10 #Пороговая плотность пространства
+        nearest_agents2 = self.model.get_agents_of_type(PedestrianAgent).select(lambda a: self.get_distance(a.x, self.x, a.y, self.y) < R and (a.x!=self.x or a.y!=self.y))
+            
+        if(len(nearest_agents2)<=po):
+            self.human = pygame.image.load('human.png')
+        if(len(nearest_agents2)>po):
+            self.human = pygame.image.load('human2.png')
+
+        #Движение пешехода на вход до прохождения турникета
+        if self.type == 1 and self.state==0:
+            irez = int(random.random()* len(self.model.arr_list1))
+            alpha = atan2(self.model.arr_list1[irez-1] - self.y, 400 - self.x)
+            
+            #Отбираем близко расположенных агентов
+            nearest_agents = self.model.get_agents_of_type(PedestrianAgent).select(lambda a: self.get_distance(a.x, self.x, a.y, self.y) < 10 and a.type==self.type and (a.x!=self.x or a.y!=self.y))
+            
+            if(len(nearest_agents)>0):
+                min_dist = 10000
+                for i in range(0, len(nearest_agents)):
+                    if self.get_distance(nearest_agents[i].x, self.x, nearest_agents[i].y, self.y) < min_dist:
+                         min_dist = self.get_distance(nearest_agents[i].x, self.x, nearest_agents[i].y, self.y)
+                         nearest_agent_index = i # Ближайший агент
+                
+
+            #Если нет препятствий в виде других агентов, то агент движется к турникету
+            if(len(nearest_agents)==0):
+                self.x = self.x + 1*cos(alpha)
+                self.y = self.y + 1*sin(alpha)
+                
+            #Если есть препятствие в виде других агентов, то агент смещается в противоположную сторону
+            if(len(nearest_agents)>0):
+                gamma = math.pi + atan2(nearest_agents[nearest_agent_index].y - self.y, nearest_agents[nearest_agent_index].x - self.x)
+                self.x = self.x + min_dist*cos(gamma)
+                self.y = self.y + min_dist*sin(gamma) 
+            
+        #Движение пешехода на выход до прохождения турникета
+        if self.type == 2  and self.state==0:
+            irez = int(random.random()* len(self.model.arr_list2))
+            alpha = atan2(self.model.arr_list2[irez-1] - self.y, 400 - self.x)
+            
+            #Отбираем близко расположенных агентов
+            nearest_agents = self.model.get_agents_of_type(PedestrianAgent).select(lambda a: self.get_distance(a.x, self.x, a.y, self.y) < 10 and a.type==self.type and (a.x!=self.x or a.y!=self.y))
+            
+            if(len(nearest_agents)>0):
+                min_dist = 10000
+                for i in range(0, len(nearest_agents)):
+                    if self.get_distance(nearest_agents[i].x, self.x, nearest_agents[i].y, self.y) < min_dist:
+                         min_dist = self.get_distance(nearest_agents[i].x, self.x, nearest_agents[i].y, self.y)
+                         nearest_agent_index = i # Ближайший агент
+                         
+                
+            #Если нет препятствий в виде других агентов, то агент движется к турникету
+            if(len(nearest_agents)==0):
+                self.x = self.x + 1*cos(alpha)
+                self.y = self.y + 1*sin(alpha)
+                
+            #Если есть препятствие в виде других агентов, то агент смещается в противоположную сторону
+            if(len(nearest_agents)>0):
+                gamma = math.pi + atan2(nearest_agents[nearest_agent_index].y - self.y, nearest_agents[nearest_agent_index].x - self.x)
+                self.x = self.x + min_dist*cos(gamma)
+                self.y = self.y + min_dist*sin(gamma) 
+
+        #Движение пешехода на вход после прохождения турникета
+        if self.type == 1 and self.state==1:
+            self.x = self.x + 1
+        #Движение пешехода на выход после прохождения турникета
+        if self.type == 2  and self.state==1:
+            self.x = self.x - 1
+        
+        #Смена состояния агента при проходе через турникет
+        if (self.x > 400 and self.type == 1) or (self.x < 400 and self.type == 2):
+            self.state = 1
+    
+    # Расчёт Евклидова расстояния     
+    def get_distance(self, x1, x2, y1, y2):
+        dx = x1 - x2
+        dy = y1 - y2
+        return math.sqrt(dx**2 + dy**2)
+
+       
+class TurnstileAgentIn(mesa.Agent): #Турникеты на вход
+    """An agent with fixed initial wealth."""
+    # Списки y-координат турникетов
+
+    def __init__(self, unique_id, model):
+        # Pass the parameters to the parent class.
+        super().__init__(unique_id, model)
+
+        # Create the agent's attribute and set the initial values.
+        self.turn = pygame.image.load('turn1.png')
+        self.x = 0
+        self.y = 0
+        
+class TurnstileAgentOut(mesa.Agent): #Турникеты на выход
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, unique_id, model):
+        # Pass the parameters to the parent class.
+        super().__init__(unique_id, model)
+
+        # Create the agent's attribute and set the initial values.
+        self.turn = pygame.image.load('turn2.png')
+        self.x = 0
+        self.y = 0
+
+
+class TurnstileModel(mesa.Model): #Класс модели
+    """A model with some number of agents."""
+    
+    # Списки координат турникетов
+    arr_list1 = list()
+    arr_list2 = list()
+
+    def __init__(self, N1, N2, N3, dist, width, height):
+        super().__init__()
+
+        self.num_agents1 = N1
+        self.num_agents2 = N2
+        self.num_agents3 = N3
+        
+        
+        # Create scheduler and assign it to the model
+        self.schedule = mesa.time.RandomActivation(self)
+        
+        self.space = mesa.space.ContinuousSpace(width, height, True)
+        
+        
+        # Create agents
+        for i in range(self.num_agents1):
+                        
+            # Add the agent to a space
+            p = self.random.random()
+            if p < 0.5:
+                   x_min = 10
+                   y_min  = 200
+                   x_max = self.space.x_max / 2
+                   y_max = self.space.y_max - 300
+                   a = PedestrianAgent(i, 1, self)
+                  
+            if p >= 0.5:
+                   x_min = self.space.x_max / 2 
+                   y_min  = 200
+                   x_max = self.space.x_max - 700
+                   y_max = self.space.y_max - 300
+                   a = PedestrianAgent(i, 2, self)
+                   
+            x = x_min + self.random.random() * x_max
+            y = y_min + self.random.random() * y_max
+            a.x = x
+            a.y = y
+            
+            # Add the agent to the scheduler
+            self.schedule.add(a)
+            
+            pos = np.array((x, y))
+                        
+            self.space.place_agent(a, pos)
+            
+        y = 160
+        for i in range(self.num_agents2):
+                    a = TurnstileAgentIn(i, self)
+                    # Add the agent to the scheduler
+                    self.schedule.add(a)
+            
+                    # Add the agent to a space
+                    x = 400
+                    y = y + dist;
+                    self.arr_list1.insert(len(self.get_agents_of_type(TurnstileAgentIn)) + 1, y)
+                    a.x = x
+                    a.y = y
+                    pos = np.array((x, y))
+            
+                    self.space.place_agent(a, pos)
+                    
+        y = 460
+        for i in range(self.num_agents3):
+                    a = TurnstileAgentOut(i, self)
+                    # Add the agent to the scheduler
+                    self.schedule.add(a)
+            
+                    # Add the agent to a space
+                    x = 400
+                    y = y + dist;
+                    self.arr_list2.insert(len(self.get_agents_of_type(TurnstileAgentOut)) + 1, y)
+                    a.x = x
+                    a.y = y
+                    pos = np.array((x, y))
+            
+                    self.space.place_agent(a, pos)
+
+    def step(self):
+        """Advance the model by one step."""
+
+        # The model's step will go here for now this will call the step method of each agent and print the agent's unique_id
+      
+        self.schedule.step() 
+        
+    def add_agent1(self):
+
+             p = self.random.random()
+             if p < 0.5:
+                   x_min = 10
+                   y_min = 200 
+                   x_max = self.space.x_max / 2
+                   y_max = self.space.y_max - 300
+                   a = PedestrianAgent(len(self.get_agents_of_type(PedestrianAgent)) + 1, 1, self)
+                  
+             if p >= 0.5:
+                   x_min = self.space.x_max / 2
+                   y_min = 200
+                   x_max = self.space.x_max - 700
+                   y_max = self.space.y_max - 300
+                   a = PedestrianAgent(len(self.get_agents_of_type(PedestrianAgent)) + 1, 2, self)
+                   
+             x = x_min + self.random.random() * x_max
+             y = y_min + self.random.random() * y_max
+             a.x = x
+             a.y = y
+             pos = np.array((x, y))
+             
+             self.schedule.add(a)
+             self.space.place_agent(a, pos)
+             
+    def add_agent2(self, y, d):
+
+             a = TurnstileAgentIn(len(self.get_agents_of_type(TurnstileAgentIn)) + 1, self)
+             self.schedule.add(a)
+             
+             x = 400
+             y = y + d
+             
+             self.arr_list1.insert(len(self.get_agents_of_type(TurnstileAgentIn)) + 1, y)
+             
+             a.x = x
+             a.y = y
+             pos = np.array((x, y))
+             self.space.place_agent(a, pos)
+             
+             
+    def add_agent3(self, y, d):
+
+             a = TurnstileAgentOut(len(self.get_agents_of_type(TurnstileAgentOut)) + 1, self)
+             self.schedule.add(a)
+             
+             x = 400
+             y = y + d
+             
+             self.arr_list2.insert(len(self.get_agents_of_type(TurnstileAgentOut)) + 1, y)
+
+             a.x = x
+             a.y = y
+             pos = np.array((x, y))
+             self.space.place_agent(a, pos)
+             
